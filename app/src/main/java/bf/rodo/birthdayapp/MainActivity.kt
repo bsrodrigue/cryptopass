3bc61a4b2aab44518b9a801a03d07ad39dac3ae3
BADINI Rachid Rodrigue <bsrodrigue@gmail.com>
2024-09-03 16:01:01 +0000

    first commit

---
+++ app/src/main/java/bf/rodo/birthdayapp/MainActivity.kt
@@ -0,0 +1,252 @@
+package bf.rodo.birthdayapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import bf.rodo.birthdayapp.ui.theme.BirthdayAppTheme
+
+val charsetMap: Map<String, List<Char>> = mapOf(
+    "uppercase" to ('A'..'Z').toList(),
+    "lowercase" to ('a'..'z').toList(),
+    "digits" to ('0'..'9').toList(),
+    "special" to listOf('!', '@', '#', '$', '%', '^', '&', '*')
+)
+
+fun getCharset(charsetNames: List<String>): List<Char> {
+    val charList = mutableListOf<Char>()
+
+    for (name in charsetNames) {
+        charsetMap[name]?.let {
+            charList.addAll(it)
+        }
+    }
+
+    return charList
+}
+
+fun genPassword(length: Int, charset: List<Char>): String {
+    return (1..length)
+        .map { charset.random() }
+        .joinToString("")
+}
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            var passwordLength by remember { mutableStateOf("") }
+            var isActiveDigits by remember { mutableStateOf(false) }
+            var isActiveUppercase by remember { mutableStateOf(false) }
+            var isActiveLowercase by remember { mutableStateOf(false) }
+            var isActiveSpecial by remember { mutableStateOf(false) }
+
+            var passwords by remember {
+                mutableStateOf(
+                    listOf(
+                        genPassword(
+                            10,
+                            getCharset(listOf("uppercase", "lowercase", "digits", "special"))
+                        ),
+                    )
+                )
+            }
+
+            BirthdayAppTheme(darkTheme = false, dynamicColor = false) {
+                Scaffold(
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {
+                            }
+                        ) {
+                            Text("Create Password")
+                        }
+                    },
+
+                    content = { paddingValues ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(paddingValues)
+                        ) {
+                            Column(modifier = Modifier.fillMaxSize()) {
+                                PasswordList(items = passwords, onClickDelete = { it ->
+                                    val password = it
+                                    passwords = passwords.filter { it ->
+                                        it != password
+                                    }
+                                })
+                                TextField(
+                                    modifier =
+                                    Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = Dp(10f)),
+                                    keyboardOptions = KeyboardOptions(
+                                        keyboardType = KeyboardType.Number,
+                                    ),
+                                    singleLine = true,
+                                    label = {
+                                        Text("Password Length")
+                                    },
+                                    placeholder = {
+                                        Text("Enter length of your password")
+                                    },
+                                    value = passwordLength, onValueChange = {
+                                        passwordLength = it
+                                    })
+
+                                CustomCheckbox(
+                                    "Digit (numbers)",
+                                    checked = isActiveDigits,
+                                    onCheckedChange = {
+                                        isActiveDigits = it
+                                    },
+                                )
+
+                                CustomCheckbox("Uppercase",
+                                    checked = isActiveUppercase,
+                                    onCheckedChange = {
+                                        isActiveUppercase = it
+                                    })
+
+                                CustomCheckbox("Lowercase",
+                                    checked = isActiveLowercase,
+                                    onCheckedChange = {
+                                        isActiveLowercase = it
+                                    })
+
+                                CustomCheckbox("Special", checked = isActiveSpecial,
+                                    onCheckedChange = {
+                                        isActiveSpecial = it
+                                    })
+
+                                Button(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    onClick = {
+                                        fun onClick() {
+
+                                            var charsetList: MutableList<String> = mutableListOf()
+
+                                            if (isActiveDigits) charsetList.add("digits")
+                                            if (isActiveUppercase) charsetList.add("uppercase")
+                                            if (isActiveLowercase) charsetList.add("lowercase")
+                                            if (isActiveSpecial) charsetList.add("special")
+
+                                            if (charsetList.size == 0 || passwordLength.isEmpty()) return
+
+                                            val password = genPassword(
+                                                passwordLength.toInt(),
+                                                getCharset(
+                                                    charsetList.toList()
+                                                )
+                                            )
+
+                                            passwords = passwords + password
+                                        }
+
+                                        onClick()
+                                    }
+                                ) {
+                                    Text("Create password")
+                                }
+                            }
+
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CustomCheckbox(label: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = {
+                onCheckedChange(it)
+            }
+        )
+        Text(label)
+    }
+
+}
+
+@Composable
+fun PasswordList(items: List<String>, onClickDelete: (String) -> Unit) {
+    LazyColumn {
+        items(items) { item ->
+            ListItem(text = item, onClickDelete)
+        }
+    }
+}
+
+@Composable
+fun ListItem(text: String, onClickDelete: (String) -> Unit) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(text = "Delete", style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.clickable {
+                        onClickDelete(text)
+                    }
+                )
+            }
+
+
+        }
+    }
+}
